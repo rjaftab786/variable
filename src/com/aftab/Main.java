f5a4abecd1ac15f0f1af93d9072143ddb8d61e35
Aftab <31744712+rjaftab786@users.noreply.github.com>
2022-04-05 20:30:17 +0530

    variables

---
+++ src/com/aftab/Main.java
@@ -0,0 +1,37 @@
+package com.aftab;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        System.out.print(" *\n ** \n *** \n **** \n *****");
+
+//        int a = 10;
+//        int b = 5;
+//
+//        int ans = (a * b) / (a - b);
+//
+//        System.out.println(ans);
+
+//        input
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        String name = sc.nextLine();
+//
+//        System.out.println(name);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+    }
+}
